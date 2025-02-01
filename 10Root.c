c92c7fb6cd0ab0f77e04f652722bd9cf93e106af
Anurag1742 <anuragranjan373@gmail.com>
2025-02-01 19:39:08 +0530

    Create 10Root.c

---
+++ 10Root.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+
+    printf("Square root: %f", sqrt(num));
+
+    getch();
+    return 0;
+}
