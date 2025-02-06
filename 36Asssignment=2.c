80985fcb7e0ac0f10ac4e78611a08c1ce3098eaa
Anurag1742 <anuragranjan373@gmail.com>
2025-02-06 11:23:20 +0530

    Create 36Asssignment=2.c

---
+++ 36Asssignment=2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a,b,c,d,e,f;
+    a = b = c = d = e = f = 10;
+    
+    printf("\n a = %d, b = %d", a, b);
+    printf("\n c = %d, d = %d", c, d);
+    printf("\n e = %d, f = %d", e, f);
+
+    getch();
+    return 0;
+}
