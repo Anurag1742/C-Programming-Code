7c4849e2d13596c2c7d7ee925b9fa8beeb08cfdb
Anurag1742 <anuragranjan373@gmail.com>
2025-02-12 23:42:14 +0530

    Create 55KM=M-CM-MM.c

---
+++ if-else/55KM=M-CM-MM.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    float km;
+    int v;
+    float ans;
+    printf("Enter distance in K.M : ");
+    scanf("%f", &km);
+
+    printf("1 for meter 2 for centimer 3 for mm : ");
+    fflush(stdin);
+    scanf("%d", &v);
+
+    if (v==1){
+        ans = km * 1000;
+    }
+
+    if (v==2){
+        ans = km * 100000;
+    }
+
+    if(v==3){
+        ans = km * 1000000;
+    }
+
+    printf("Answer : %.2f", ans);
+    getch();
+}
