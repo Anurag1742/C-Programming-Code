478c0bb7d7601b1d3047baa6f108089be75b9f30
Anurag1742 <anuragranjan373@gmail.com>
2025-02-17 23:21:18 +0530

    Create 63Conditional_Operator.c

---
+++ if-else/63Conditional_Operator.c
@@ -0,0 +1,10 @@
+//Find maximum among three using conditional operator
+
+#include<stdio.h>
+void main()
+{
+     int a,b,c;
+     printf("Enter three numbers : "); 
+     scanf("%d%d%d",&a,&b,&c);
+     printf("Max is: %d",a>b?a>c?a:c:b>c?b:c);
+}
