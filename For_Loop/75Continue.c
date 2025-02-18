71e004206c86fa29c8e441358f9854d1927d6b0a
Anurag1742 <anuragranjan373@gmail.com>
2025-02-18 18:57:58 +0530

    Create 75Continue.c

---
+++ For_Loop/75Continue.c
@@ -0,0 +1,21 @@
+// continue statement
+
+#include <stdio.h> 
+#include <conio.h>
+ 
+int  main() 
+{ 
+	int i;
+
+	for( i=1 ; i<=10 ; i++ )
+	{
+		if(i == 5)
+		{
+			continue;
+		}
+		printf( "  %d", i );
+	}
+
+	getch();
+	return 0;
+}
